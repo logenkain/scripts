92b63299a5d5cd62938f2a4cd0160784a4b7fdc0
Logen Kain <logen@sudotask.com>
2016-09-16 18:54:39 -0700

    Updated Dins link

--- DinsCurse.sh
+++ DinsCurse.sh
@@ -1,1 +1,4 @@
-rsync -rtv /home/logen/.local/DinsCurse/ /home/logen/.local/DinsCurse.bak2/
+#!/bin/sh
+/bin/bash /home/logen/GOG\ Games/Dins\ Curse/start.sh;
+sleep 5;
+rsync -rtv /home/logen/.local/DinsCurse/ /home/logen/.local/DinsCurse.bak2/ ;
